3bf72d630517afc57fe0e6e922d6171c99cf2676
toufiqnuur <taufik2017yk@gmail.com>
2022-12-21 20:23:00 +0700

    add filter category

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { retrieveThreads } from "../store/shared/action";
 import Container from "../components/Container";
@@ -6,6 +6,7 @@ import Thread from "../components/Thread";
 
 export default function HomePage() {
   const { threads } = useSelector((state) => state);
+  const [filterCategory, setFilterCategory] = useState(null);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,10 +15,32 @@ export default function HomePage() {
 
   return (
     <Container className="pt-6 pb-24">
-      <h3>Diskusi tersedia</h3>
-      {threads.map((thread) => (
-        <Thread content={thread} key={thread.id} />
-      ))}
+      <h3>Daftar kategori</h3>
+      <div className="mt-3 flex flex-wrap gap-3">
+        {[...new Set(threads.map((thread) => thread.category))].map(
+          (category, index) => (
+            <button
+              className={`badge__category ${
+                filterCategory === category && "border-blue-600 text-blue-600"
+              }`}
+              onClick={() =>
+                setFilterCategory(filterCategory === category ? null : category)
+              }
+              key={index}
+            >
+              {category}
+            </button>
+          )
+        )}
+      </div>
+      <h3 className="mt-6">Diskusi tersedia</h3>
+      {threads
+        .filter((thread) =>
+          filterCategory ? thread.category === filterCategory : thread
+        )
+        .map((thread) => (
+          <Thread content={thread} key={thread.id} />
+        ))}
     </Container>
   );
 }
