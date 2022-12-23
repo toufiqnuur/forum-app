79d8cd61a10e88651421f240f769ac2f994056ff
toufiqnuur <taufik2017yk@gmail.com>
2022-12-23 14:01:23 +0700

    done

---
+++ src/states/leaderboards/action.js
@@ -0,0 +1,30 @@
+import { hideLoading, showLoading } from "react-redux-loading-bar";
+import api from "../../utils/apis";
+
+const ActionType = {
+  SET_LEADERBOARDS: "SET_LEADERBOARDS",
+};
+
+function setLeaderboardsTrigger(leaderboards) {
+  return {
+    type: ActionType.SET_LEADERBOARDS,
+    payload: {
+      leaderboards,
+    },
+  };
+}
+
+function asyncGetLeaderboards() {
+  return async (dispatch) => {
+    dispatch(showLoading());
+    try {
+      const leaderboards = await api.getLeaderboards();
+      dispatch(setLeaderboardsTrigger(leaderboards));
+    } catch (error) {
+      alert(error.message);
+    }
+    dispatch(hideLoading());
+  };
+}
+
+export { ActionType, setLeaderboardsTrigger, asyncGetLeaderboards };
