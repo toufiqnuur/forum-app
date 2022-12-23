98a7e240df4d75a56b14bd89f2675aaa76a2550c
toufiqnuur <taufik2017yk@gmail.com>
2022-12-23 16:15:58 +0700

    done

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,46 +1,39 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { retrieveThreads } from "../store/shared/action";
+import CategoryList from "../components/CategoryList";
 import Container from "../components/Container";
-import Thread from "../components/Thread";
+import ThreadList from "../components/ThreadList";
+import {
+  asyncGetThreadsAndUsers,
+  asyncToggleDislikeThread,
+  asyncToggleLikeThread,
+} from "../states/threads/action";
 
 export default function HomePage() {
+  const dispatch = useDispatch();
   const { threads } = useSelector((state) => state);
   const [filterCategory, setFilterCategory] = useState(null);
-  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(retrieveThreads());
+    dispatch(asyncGetThreadsAndUsers());
   }, [dispatch]);
 
   return (
     <Container className="pt-6 pb-24">
       <h3>Daftar kategori</h3>
-      <div className="mt-3 flex flex-wrap gap-3">
-        {[...new Set(threads.map((thread) => thread.category))].map(
-          (category, index) => (
-            <button
-              className={`badge__category ${
-                filterCategory === category && "border-blue-600 text-blue-600"
-              }`}
-              onClick={() =>
-                setFilterCategory(filterCategory === category ? null : category)
-              }
-              key={index}
-            >
-              {category}
-            </button>
-          )
-        )}
-      </div>
+      <CategoryList
+        categories={[...new Set(threads.map((thread) => thread.category))]}
+        current={filterCategory}
+        onChange={(selected) => setFilterCategory(selected)}
+      />
       <h3 className="mt-6">Diskusi tersedia</h3>
-      {threads
-        .filter((thread) =>
+      <ThreadList
+        threads={threads.filter((thread) =>
           filterCategory ? thread.category === filterCategory : thread
-        )
-        .map((thread) => (
-          <Thread content={thread} key={thread.id} />
-        ))}
+        )}
+        onUpVote={(id) => dispatch(asyncToggleLikeThread(id))}
+        onDownVote={(id) => dispatch(asyncToggleDislikeThread(id))}
+      />
     </Container>
   );
 }
