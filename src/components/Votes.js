6e881a34a76fab092a2b2e51d9d5f35e397d7ce8
toufiqnuur <taufik2017yk@gmail.com>
2022-12-23 15:48:48 +0700

    done

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,19 +1,39 @@
 import { IoThumbsDownOutline, IoThumbsUpOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
 
 export default function Votes({
+  threadId,
   upVote,
   downVote,
   onUpVote,
   onDownVote,
   className,
 }) {
+  const { authUser } = useSelector((state) => state);
+
   return (
     <div className={`inline-flex space-x-3 ${className}`}>
-      <button className="inline-flex space-x-1" onClick={onUpVote}>
-        <IoThumbsUpOutline /> <span>{upVote}</span>
+      <button
+        className="inline-flex space-x-1"
+        onClick={() => onUpVote(threadId)}
+      >
+        <IoThumbsUpOutline
+          className={`${
+            authUser && upVote.includes(authUser.id) && "text-blue-600"
+          }`}
+        />{" "}
+        <span>{upVote?.length}</span>
       </button>
-      <button className="inline-flex space-x-1" onClick={onDownVote}>
-        <IoThumbsDownOutline /> <span>{downVote}</span>
+      <button
+        className="inline-flex space-x-1"
+        onClick={() => onDownVote(threadId)}
+      >
+        <IoThumbsDownOutline
+          className={`${
+            authUser && downVote.includes(authUser.id) && "text-blue-600"
+          }`}
+        />{" "}
+        <span>{downVote?.length}</span>
       </button>
     </div>
   );
