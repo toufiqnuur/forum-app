0a37d2b8bfc3e494f40258bb6dfa15aab0b0a815
toufiqnuur <taufik2017yk@gmail.com>
2022-12-26 13:09:01 +0700

    done

---
+++ src/components/EditableInput.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+export default function EditableInput({
+  initial,
+  disabled,
+  reset,
+  onChange,
+  onFocus,
+}) {
+  const wysiwyg = useRef("");
+
+  useEffect(() => {
+    if (wysiwyg.current) {
+      if (reset) {
+        wysiwyg.current.innerHTML = null;
+      }
+    }
+  }, [reset]);
+
+  return (
+    <div
+      ref={wysiwyg}
+      className="form-textarea mt-3 min-h-[100px] w-full overflow-scroll"
+      onInput={(e) => onChange(e.currentTarget.innerHTML)}
+      onFocus={onFocus}
+      contentEditable={!disabled}
+      suppressContentEditableWarning
+    >
+      {initial}
+    </div>
+  );
+}
